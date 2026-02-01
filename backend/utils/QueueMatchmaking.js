ec1208bbf562d81868cd43e947c4501529b75ecc
Aryan Khaire <aryan.khaire001@gmail.com>
2026-02-01 19:06:19 +0530

    Matchmaking Queue implemented

--- backend/utils/QueueMatchmaking.js
+++ backend/utils/QueueMatchmaking.js
@@ -12,8 +12,21 @@ const QueueMatchmaking = {
     },
 
     matchUser : function(){
+        while(this.queue.length >= 2){
+            const user1 = this.queue.shift();
+            const user2 = this.queue.shift();
 
+            const roomId = `room_${user1.id}_${user2.id}`;
+            user1.emit("match_found", { 
+                roomId, 
+                partnerNickname: user2.handshake.query.nickname || "Stranger" 
+            });
+            user2.emit("match_found", { 
+                roomId, 
+                partnerNickname: user1.handshake.query.nickname || "Stranger" 
+            });
+        }
     }
 }
 
-module.exports = {QueueMatchmaking};
+module.exports = {QueueMatchmaking: new QueueMatchmaking()};
